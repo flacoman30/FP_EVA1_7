a1bccfdc707e562442a97a760a1378994a37e28d
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-09-04 16:50:35 -0600

    Add files via upload

---
+++ EVA1_7_OPERACIONES/EVA1_7_OPERACIONES/src/main/java/com/mycompany/eva1_7_operaciones/EVA1_7_OPERACIONES.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_7_operaciones;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA1_7_OPERACIONES {
+
+    public static void main(String[] args) {
+    //OPERACIONES IGUALES CO MO LAS MATEMATICAS
+    //TODOS CODE APPLICATION LOGIC HERE
+    //OPERACIONES ARITMETICAS
+    int x = 10; //declaracion y asignacion;
+    int y = 5;
+    //declaracion de mutiples variables del mismi tipo
+    int suma, resta, divi, multi, pot, raiz;
+    //suma
+    suma = x + y; //operador de suma+
+    System.out.println("suma de x + y");
+    System.out.println(suma);
+    //resta
+    resta = x - y; //operacion de diferencia
+    System.out.println("resta de x - y");
+    System.out.println(resta); 
+    //multiplicacion
+    multi = x * y;
+    System.out.println("multiplicacion x * y");
+    System.out.println(multi);
+    
+    divi = x / y;
+    //division---)el manejo de tipo de datos
+    //si dividimos enteros java da como resultado un enteros
+    x = 5;
+    y = 2;
+    System.out.println("division x(5) / y (2)");
+    System.out.println(divi);
+    
+    double vall = 11;
+    double va12 = 3;
+    double diviD;
+    diviD = vall / va12;
+    System.out.println("Division entre # decimal");
+    System.out.println(diviD);
+    
+    
+    
+            
+    
+    
+    
+    }
+}
